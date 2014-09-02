e8e6a09b9e48e398f73c9c73e058bc14cf8a238f
ahmedalaa3307023 <ahmedalaa3307023@gmail.com>
2014-09-02 17:25:35 +0300

    finishing the basic requrement

--- Mentor/src/com/mentor/app/MainWindow.java
+++ Mentor/src/com/mentor/app/MainWindow.java
@@ -1,6 +1,6 @@
 package com.mentor.app;
 	
-	import java.awt.Color;
+import java.awt.Color;
 import java.awt.Component;
 import java.awt.Dimension;
 import java.awt.EventQueue;
@@ -11,21 +11,18 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
+import java.io.BufferedReader;
 import java.io.ByteArrayInputStream;
-import java.io.IOException;
 import java.io.InputStream;
-import java.sql.SQLException;
+import java.io.InputStreamReader;
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Date;
-	
+import java.util.List;
 
-
-
-	import javax.swing.ImageIcon;
+import javax.swing.ImageIcon;
 import javax.swing.JButton;
-import javax.swing.JEditorPane;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JMenu;
@@ -33,30 +30,36 @@ import javax.swing.JMenuBar;
 import javax.swing.JMenuItem;
 import javax.swing.JPanel;
 import javax.swing.JPasswordField;
-import javax.swing.JScrollPane;
 import javax.swing.JTextField;
 import javax.swing.JTextPane;
 import javax.swing.SwingConstants;
 import javax.swing.Timer;
 import javax.swing.UIManager;
 import javax.swing.border.CompoundBorder;
-	
 
+import org.apache.http.HttpResponse;
+import org.eclipse.wb.swing.FocusTraversalOnArray;
+import org.json.JSONArray;
+import org.json.JSONObject;
+import org.apache.http.NameValuePair;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.entity.UrlEncodedFormEntity;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.message.BasicNameValuePair;
 
-
-	import org.eclipse.wb.swing.FocusTraversalOnArray;
-	
-
-
-
-	import com.jcraft.jsch.Channel;
+import com.jcraft.jsch.Channel;
 import com.jcraft.jsch.JSch;
 import com.jcraft.jsch.JSchException;
 import com.jcraft.jsch.Session;
 import com.jcraft.jsch.UserInfo;
-import com.mentor.app.database.DatabaseConnector;
 import com.mentor.app.database.helpers.MyUserInfo;
-import com.mentor.app.database.helpers.UserData;
+import com.mentor.app.database.helpers.Tool;
+import javax.swing.JList;
+import javax.swing.JCheckBoxMenuItem;
+import javax.swing.JCheckBox;
+import javax.swing.JPopupMenu;
+import javax.swing.JComboBox;
 	
 	public class MainWindow {
 	
@@ -97,6 +100,7 @@ import com.mentor.app.database.helpers.UserData;
 		private JLabel emailHint;
 		private JLabel userNameHint;
 		private JPanel toolsList;
+		private JPanel registerationPanel;
 		
 		//////////////////////////////////////////////////
 		public static void main(String[] args) {
@@ -118,23 +122,43 @@ import com.mentor.app.database.helpers.UserData;
 		 */
 		public MainWindow() {
 	
-			Toolkit tk = Toolkit.getDefaultToolkit();
-			mainWindowSize = tk.getScreenSize();
+			
+			Toolkit tk = Toolkit.getDefaultToolkit();  
+			/*
+			 * toolkit is used to get current information form the targent plateform and devices
+			 * the use of toolkit is to get the current size of the screen to adjust the frame to it
+			 * the concept used in order to use this toolkit is to avoid modifying component places when the use resize the window
+			 */
+			mainWindowSize = tk.getScreenSize(); // getting the screen size and save it to a private variable to use it  the size is saved as a Dimension type
 			
 			//mainPanelIntial();
+			
 			frmMentorApplication = new JFrame();
 			frmMentorApplication.setTitle("Mentor Application");
+			/*
+			 * the link below is the current link for the logo image can be modefied any time
+			 */
 			frmMentorApplication.setIconImage(Toolkit.getDefaultToolkit().getImage("C:\\Users\\amohamed\\Desktop\\cloud-models-100255369-orig.jpg"));
 			frmMentorApplication.getContentPane().setBackground(new Color(153, 153, 153));
-			frmMentorApplication.getContentPane().setLayout(null);
-			mainPanel = new JPanel();
+			frmMentorApplication.getContentPane().setLayout(null); // null layout = abslute layout
+			
+			// the main panel cantains all other panels and options
+			mainPanel = new JPanel(); 
 			mainPanel.setForeground(UIManager.getColor("CheckBox.focus"));
 			mainPanel.setBackground(new Color(255, 255, 255));
 			mainPanel.setBounds(0, 0, 1594, 1172);
 			frmMentorApplication.getContentPane().add(mainPanel);
 			mainPanel.setLayout(null);
+			
+			////////////////////////////////////////////////////////////////////////////////////
+			// used data to get the client current time
 			DateFormat dateFormat = new SimpleDateFormat("yyyy/MM/dd   HH:mm:ss");
 			Date date = new Date();
+			///////////////////////////////////////////////////////////////////////////////////
+			
+			//////////////////////////////////////////////////////////////////////////////////
+			// start editing the timer viewer label
+			// the lable used to view the time
 			timeViewer = new JLabel((dateFormat.format(date).toString()));
 			timeViewer.setHorizontalAlignment(SwingConstants.CENTER);
 			timeViewer.setFont(new Font("Serif", Font.BOLD, 16));
@@ -142,7 +166,11 @@ import com.mentor.app.database.helpers.UserData;
 			timeViewer.setBackground(new Color(192, 192, 192));
 			timeViewer.setBounds(1396, 0, 198, 20);
 			mainPanel.add(timeViewer);
+			// finish editing the timer viewer label
+			////////////////////////////////////////////////////////////////////////////////
 			
+			///////////////////////////////////////////////////////////////////////////////
+			// view of the upper menu 
 			menuBar = new JMenuBar();
 			menuBar.setForeground(Color.WHITE);
 			menuBar.setBorderPainted(false);
@@ -193,36 +221,26 @@ import com.mentor.app.database.helpers.UserData;
 			mntmPricing = new JMenuItem("Pricing");
 			buyMenu.add(mntmPricing);
 			
-			logoLabel = new JLabel("");
-			buyMenu.add(logoLabel);
-			logoLabel.setVerticalAlignment(SwingConstants.TOP);
-			logoLabel.setLabelFor(mainPanel);
-			logoLabel.setIcon(new ImageIcon("C:\\Users\\amohamed\\Desktop\\mentor-graphics-logo.jpg"));
-			
 			mntmHowToPay = new JMenuItem("How to pay");
 			buyMenu.add(mntmHowToPay);
+			// finish edting the upper menu
+			/////////////////////////////////////////////////////////////////////
 			
+			/////////////////////////////////////////////////////////////////////
+			// the main registerration button on the left of the screen
 			JButton registerButton = new JButton("Register");
+			
+			registerButton.setEnabled(false);
 			registerButton.addActionListener(new ActionListener() {
 				public void actionPerformed(ActionEvent e) {
 					loginPanel.setVisible(false);
-					String url = "http://www.google.com";
-				    JEditorPane editor;
 					try {
-						editor = new JEditorPane(url);
-						editor.setEditable(false);
 						
-						JScrollPane pane = new JScrollPane(editor);
-						pane.setSize(1500, 1500);
-						pane.show();
-						mainPanel.add(pane);
-						 System.out.println("hhhhhhhhhhhhhhhhhhhhhhhhhhh");
-					} catch (IOException e1) {
+						//register();
+					} catch (Exception e1) {
 						// TODO Auto-generated catch block
 						e1.printStackTrace();
 					}
-				   
-				    
 				}
 			});
 			registerButton.setBackground(new Color(255, 255, 255));
@@ -261,17 +279,26 @@ import com.mentor.app.database.helpers.UserData;
 			aboutProductLabel.setBounds(10, 579, 124, 20);
 			mainPanel.add(aboutProductLabel);
 			
-			toolsList = new JPanel();
-			toolsList.setBounds(801, 133, 658, 739);
-			mainPanel.add(toolsList);
-			toolsList.setBackground(UIManager.getColor("Button.disabledShadow"));
-			toolsList.setLayout(null);
+			aboutProductField = new JTextPane();
+			aboutProductField.setText("This is Area for small overview about the product and the developers");
+			aboutProductField.setForeground(new Color(0, 0, 128));
+			aboutProductField.setFont(new Font("Trebuchet MS", Font.BOLD, 16));
+			aboutProductField.setBackground(Color.WHITE);
+			aboutProductField.setBounds(10, 618, 527, 102);
+			mainPanel.add(aboutProductField);
+			
+			logoLabel = new JLabel("");
+			logoLabel.setBounds(49, 32, 240, 160);
+			mainPanel.add(logoLabel);
+			logoLabel.setVerticalAlignment(SwingConstants.TOP);
+			logoLabel.setLabelFor(mainPanel);
+			logoLabel.setIcon(new ImageIcon("C:\\Users\\amohamed\\Desktop\\mentor-graphics-logo.jpg"));
 			
 			//
 			//loginPanelIntial();
 			loginPanel = new JPanel();
-			loginPanel.setBounds(-36, 11, 624, 668);
-			toolsList.add(loginPanel);
+			loginPanel.setBounds(768, 96, 709, 739);
+			mainPanel.add(loginPanel);
 			loginPanel.setVisible(false);
 			loginPanel.setForeground(new Color(0, 0, 128));
 			loginPanel.setBorder(new CompoundBorder());
@@ -353,6 +380,33 @@ import com.mentor.app.database.helpers.UserData;
 			loginSubmit.addActionListener(new ActionListener() {
 				@SuppressWarnings("deprecation")
 				public void actionPerformed(ActionEvent e) {
+					try {
+						ArrayList<String> userData = new ArrayList<>();
+						userData.add(emailText.getText());
+						userData.add(userNameText.getText());
+						userData.add(String.copyValueOf(passwordText.getPassword()));
+						ArrayList<Tool> toolList = getToolsList(userData);
+						toolsList.setLayout(new GridLayout(toolList.size(), 1, 5, 25));
+						
+						JButton[] tools = new JButton [toolList.size()];
+						for(int i = 0 ; i < toolList.size() ; i++){
+							tools[i] = new JButton();
+							tools[i].setText(toolList.get(i).getTool_calling_name());
+							tools[i].setBackground(Color.white);
+							tools[i].setBorder(null);
+							tools[i].setAlignmentX(JButton.LEFT_ALIGNMENT);
+							tools[i].addActionListener(ac);
+							
+							
+							toolsList.add(tools[i]);
+						}
+						loginPanel.setVisible(false);
+						toolsList.show();
+					} catch (Exception e1) {
+						// TODO Auto-generated catch block
+						e1.printStackTrace();
+					}
+					/*
 					ArrayList<String> config  = new ArrayList<String>();
 					config.add(new String("com.mysql.jdbc.Driver"));
 					config.add(new String("jdbc:mysql://localhost:3306/aws_connection"));
@@ -402,7 +456,7 @@ import com.mentor.app.database.helpers.UserData;
 					} catch (SQLException e1) {
 						// TODO Auto-generated catch block
 						e1.printStackTrace();
-					} 
+					} */
 				}
 			});
 			loginSubmit.setFont(new Font("Times New Roman", Font.BOLD, 19));
@@ -427,14 +481,80 @@ import com.mentor.app.database.helpers.UserData;
 			loginPanel.add(userNameHint);
 			loginPanel.setFocusTraversalPolicy(new FocusTraversalOnArray(new Component[]{userNameLabel, passwordLabel, emailLabel, logoPanel, passwordText, userNameText, emailText, forgetPasswordButton, loginSubmit, emailHint, userNameHint}));
 			
-			aboutProductField = new JTextPane();
-			aboutProductField.setText("This is Area for small overview about the product and the developers");
-			aboutProductField.setForeground(new Color(0, 0, 128));
-			aboutProductField.setFont(new Font("Trebuchet MS", Font.BOLD, 16));
-			aboutProductField.setBackground(Color.WHITE);
-			aboutProductField.setBounds(10, 618, 527, 102);
-			mainPanel.add(aboutProductField);
-			//
+			toolsList = new JPanel();
+			toolsList.setBounds(801, 133, 658, 739);
+			mainPanel.add(toolsList);
+			toolsList.setBackground(UIManager.getColor("Button.disabledShadow"));
+			toolsList.setLayout(null);
+			
+			///////////////////////////////////////////////////////////////////////////////////
+			// start editing registeration panel
+			registerationPanel = new JPanel();
+			registerationPanel.setBackground(Color.WHITE);
+			registerationPanel.setBounds(721, 103, 763, 816);
+			mainPanel.add(registerationPanel);
+			registerationPanel.setLayout(null);
+			
+			JLabel userName = new JLabel("Name");
+			userName.setFont(new Font("Times New Roman", Font.PLAIN, 18));
+			userName.setBounds(84, 188, 123, 33);
+			registerationPanel.add(userName);
+			
+			JLabel userEmail = new JLabel("Email");
+			userEmail.setFont(new Font("Times New Roman", Font.PLAIN, 18));
+			userEmail.setBounds(84, 263, 84, 41);
+			registerationPanel.add(userEmail);
+			
+			JLabel conEmail = new JLabel("Confirm Email");
+			conEmail.setFont(new Font("Times New Roman", Font.PLAIN, 18));
+			conEmail.setBounds(84, 343, 120, 41);
+			registerationPanel.add(conEmail);
+			
+			JLabel password = new JLabel("Password");
+			password.setFont(new Font("Times New Roman", Font.PLAIN, 18));
+			password.setBounds(84, 430, 84, 41);
+			registerationPanel.add(password);
+			
+			JLabel conPassword = new JLabel("Confirm Password");
+			conPassword.setFont(new Font("Times New Roman", Font.PLAIN, 18));
+			conPassword.setBounds(84, 517, 144, 41);
+			registerationPanel.add(conPassword);
+			
+			JLabel registerationType = new JLabel("Registeration Type");
+			registerationType.setFont(new Font("Times New Roman", Font.PLAIN, 18));
+			registerationType.setBounds(84, 603, 144, 41);
+			registerationPanel.add(registerationType);
+			
+			textField = new JTextField();
+			textField.setBounds(335, 188, 242, 28);
+			registerationPanel.add(textField);
+			textField.setColumns(10);
+			
+			textField_1 = new JTextField();
+			textField_1.setColumns(10);
+			textField_1.setBounds(335, 263, 242, 28);
+			registerationPanel.add(textField_1);
+			
+			textField_2 = new JTextField();
+			textField_2.setColumns(10);
+			textField_2.setBounds(335, 343, 242, 28);
+			registerationPanel.add(textField_2);
+			
+			passwordField = new JPasswordField();
+			passwordField.setBounds(335, 442, 242, 29);
+			registerationPanel.add(passwordField);
+			
+			passwordField_1 = new JPasswordField();
+			passwordField_1.setBounds(335, 529, 242, 29);
+			registerationPanel.add(passwordField_1);
+			
+			JComboBox comboBox = new JComboBox();
+			comboBox.setBounds(335, 615, 242, 20);
+			comboBox.addItem(new JCheckBox());
+			registerationPanel.add(comboBox);
+			registerationPanel.setVisible(false);
+			// finish editing of registeration panel
+			//////////////////////////////////////////////////////////////////////
 		    //timingforTimerCall();
 		    t = new Timer(1000, new ActionListener() {
 				
@@ -481,4 +601,107 @@ import com.mentor.app.database.helpers.UserData;
 				}
 			}
 		};
+		private JTextField textField;
+		private JTextField textField_1;
+		private JTextField textField_2;
+		private JPasswordField passwordField;
+		private JPasswordField passwordField_1;
+	
+		private void register() throws Exception {
+			 
+			String url = "http://localhost/registeration/Registeration/welcome_controller/insert_into_table";
+	 
+			HttpClient client = new DefaultHttpClient();
+			HttpPost post = new HttpPost(url);
+			
+			post.setHeader("User-Agent", "Mozilla/5.0");
+	 
+			List<NameValuePair> urlParameters = new ArrayList<NameValuePair>();
+			urlParameters.add(new BasicNameValuePair("name", "aahmed"));
+			urlParameters.add(new BasicNameValuePair("email", "alla.ahmed45@yahoo.com"));
+			urlParameters.add(new BasicNameValuePair("password", "5050505050"));
+			urlParameters.add(new BasicNameValuePair("type", "A"));
+	 
+			post.setEntity(new UrlEncodedFormEntity(urlParameters));
+	 
+			HttpResponse response = client.execute(post);
+			System.out.println("\nSending 'POST' request to URL : " + url);
+			System.out.println("Post parameters : " + post.getEntity());
+			System.out.println("Response Code : " + 
+	                                    response.getStatusLine().getStatusCode());
+	 
+			BufferedReader rd = new BufferedReader(
+	                        new InputStreamReader(response.getEntity().getContent()));
+	 
+			StringBuffer result = new StringBuffer();
+			String line = "";
+			while ((line = rd.readLine()) != null) {
+				result.append(line);
+			}
+	 
+			System.out.println(result.toString());
+	 
+		}
+		
+		private ArrayList<Tool> getToolsList(ArrayList<String> userData) throws Exception {
+			 
+			String url = "http://localhost/registeration/Registeration/login/log_in";
+	 
+			HttpClient client = new DefaultHttpClient();
+			HttpPost post = new HttpPost(url);
+	 
+			// add header
+			post.setHeader("User-Agent", "Mozilla/5.0");
+	 
+			List<NameValuePair> urlParameters = new ArrayList<NameValuePair>();
+			urlParameters.add(new BasicNameValuePair("user_name", userData.get(1)));
+			urlParameters.add(new BasicNameValuePair("email", userData.get(0)));
+			urlParameters.add(new BasicNameValuePair("password", userData.get(2)));
+			System.out.println(userData.get(0));
+			System.out.println(userData.get(1));
+			System.out.println(userData.get(2));
+			post.setEntity(new UrlEncodedFormEntity(urlParameters));
+			
+			HttpResponse response = client.execute(post);
+							
+			BufferedReader rd = new BufferedReader(new InputStreamReader(response.getEntity().getContent()));
+			String serverResponse = rd.readLine();
+			String json = (serverResponse.substring(serverResponse.indexOf('{'), serverResponse.length()-1));
+			System.out.println(json);
+			JSONObject obj = new JSONObject(json);
+			String state = obj.getString("success");
+			
+			ArrayList<Tool> tools = null;
+			
+			if(state.equals("true")){
+				tools = new ArrayList<Tool>();
+				JSONArray arr = obj.getJSONArray("data");
+				for (int i = 0; i < arr.length(); i++)
+				{
+					Tool tool = new Tool();
+					tool.setTool_id(arr.getJSONObject(i).getString("tool_id"));
+				    tool.setTool_calling_name(arr.getJSONObject(i).getString("calling_name"));
+				    tool.setTool_name(arr.getJSONObject(i).getString("tool_name"));
+				    tools.add(tool);
+				}
+			}
+			return tools;
+		}
+		private static void addPopup(Component component, final JPopupMenu popup) {
+			component.addMouseListener(new MouseAdapter() {
+				public void mousePressed(MouseEvent e) {
+					if (e.isPopupTrigger()) {
+						showMenu(e);
+					}
+				}
+				public void mouseReleased(MouseEvent e) {
+					if (e.isPopupTrigger()) {
+						showMenu(e);
+					}
+				}
+				private void showMenu(MouseEvent e) {
+					popup.show(e.getComponent(), e.getX(), e.getY());
+				}
+			});
+		}
 	}
